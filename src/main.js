cc1398ca092ae009b8b4314036616f453d0bb1dc
jmp1234 <johnnypham944@yahoo.com>
2019-06-28 14:43:17 -0700

    created initial app layout

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./main.css";
+
+const Main = () => {
+  return <main className="main">Main Stuff</main>;
+};
+
+export default Main;
---
+++ src/sidebar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./sidebar.css";
+
+const Sidebar = () => {
+  return <aside className="sidebar">Sidebar</aside>;
+};
+
+export default Sidebar;
