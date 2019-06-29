48d823a471faffd01a18abb5cdb66aacaae9ac20
jmp1234 <johnnypham944@yahoo.com>
2019-06-28 23:07:59 -0700

    user can now send messages

--- src/containers/chatWindow.js
+++ src/containers/chatWindow.js
@@ -3,17 +3,21 @@ import store from "../store";
 import Header from "../components/header";
 import Chats from "../components/chats";
 import _ from "lodash";
-import './chatWindow.css'
+import './chatWindow.css';
+import MessageInput from '../containers/messageInput';
+
 
 const ChatWindow = ({ activeUserId }) => {
   const state = store.getState();
   const activeUser = state.contacts[activeUserId];
   const activeMsgs = state.messages[activeUserId];
+  const { typing } = state;
 
   return (
       <div className="ChatWindow">
         <Header user={activeUser} />
         <Chats messages={_.values(activeMsgs)}/>
+        <MessageInput value={typing} />
       </div>
     );
 };
